d9e8662ca949806d2012247e5f88007d80e2551d
Tal Weiss <Tweiss747@gmail.com>
2017-10-30 23:11:09 -0400

    created main function in DataSet.py and testsed reuters data

--- DataProcessor.py
+++ DataProcessor.py
@@ -166,6 +166,42 @@ class DataProcessor:
         return doc_list
 
 
+    #function that applies bm25 smoothing
+    def bm25(self,list_doc, doc_freq, term_freq_doc, query):  # This uses BM25 smoothing where k = 10
+        nr_docs = len(list_doc)
+        term_freq_query = self.process(query)
+
+        print("Query terms ", term_freq_query)
+
+        common_terms = [term for (term, freq) in term_freq_query if term in doc_freq]
+
+        similarity = {i: 0 for i in range(nr_docs)}
+
+        if len(common_terms) == 0:
+            print("Error, no common terms between query & docs")
+        else:
+            idf_query = [np.log2((1 + nr_docs) / doc_freq.get(t)) for t in common_terms]
+            print("IDF Query terms ", idf_query)
+
+            for doc in range(nr_docs):
+                tf_q = [f for (t, f) in term_freq_query if t in common_terms]
+                tf_d = []  # for all common terms, extract the frequency of the term in doc, for the documents the term doesn't appear, the frequency is 0
+                for t in common_terms:
+                    for (d, f) in term_freq_doc.get(t):
+                        if d == doc:
+                            tf_d.append(f)
+                        else:
+                            tf_d.append(0)
+
+                for c in range(len(common_terms)):
+                    k = 2
+                    tf_idf_d = tf_q[c] * (((k + 1) * tf_d[c]) / (tf_d[c] + k)) * idf_query[c]  # k=10
+                    # tf_idf_d = tf_q[c] * np.log(1 + np.log(1+tf_d[c]))*idf_query[c]
+                    similarity[doc] += tf_idf_d
+        sorted_doc_list = sorted(similarity, key=similarity.get, reverse=True)
+        return similarity, sorted_doc_list
+
+
 
 
 
--- DataSet.py
+++ DataSet.py
@@ -3,6 +3,20 @@ from nltk.corpus import reuters
 
 
 #test file where the datasets will be evaluated
+#main file
+def main():
+    reuters_texts = []
+    #Working with the first 50 files from the reuters library
+    dp = DataProcessor()
+    reuters_data = reuters.fileids()[:50]
+    for data in reuters_data:
+        file_str = "" #concatinate file to string
+        file = reuters.open(data)
+        for line in file:
+            file_str = file_str + line
+        reuters_texts.append(file_str)
+    print(reuters_texts)
+main()
 
 
 
