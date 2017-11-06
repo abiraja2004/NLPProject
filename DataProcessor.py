fe83a4c511ff7fd7c4334c8c6532b371fd2ce1c6
Tal Weiss <Tweiss747@gmail.com>
2017-11-05 19:17:02 -0500

    Bug fixes and few functions completed and re worked

--- DataProcessor.py
+++ DataProcessor.py
@@ -20,38 +20,38 @@ class DataProcessor:
         #non_punctuated_terms = [term for term in all_tokens if term not in '!.,''?/\|~ ']
 
         #strip terms w/POS with length = 1.
-        no_puncutated_pos = [(term, pos) for (term, pos) in all_pos_tags if len(pos) > 1]
+        #no_puncutated_pos = [(term, pos) for (term, pos) in all_pos_tags if len(pos) > 1]
 
         #eliminate pos terms for terms that start with a non letter
-        non_punctuated_pos = [(term,pos) for (term,pos) in no_puncutated_pos if pos[0] in string.ascii_letters]
+        non_letter_terms = [term for term in all_tokens if term[0] in string.ascii_letters]
 
-        for(term,pos) in non_punctuated_pos:
+        for term in non_letter_terms:
             if len(term) <= 1 and term in list('~><?'';:\/|{}[]@6&*-_.'):
-                non_punctuated_pos.remove((term,pos))
+                non_letter_terms.remove(term)
 
         #Now we take the terms and put them in lowercase
-        words = [(term.lower(),pos) for (term,pos) in non_punctuated_pos] #iterates through each term and converts it to lowercase
+        words = [term.lower() for term in non_letter_terms] #iterates through each term and converts it to lowercase
 
         #Snippet of code to stem the terms
         stemmer = PorterStemmer() #initialize the stemmer object
-        stemmed_words = [(stemmer.stem(w),pos) for (w,pos) in words]
+        stemmed_words = [stemmer.stem(w) for w in words]
         #print("\n stemmed words: ", stemmed_words) #used for debugging purposes
 
         #Snippet to lemmatize the words
         wl = WordNetLemmatizer()
-        lemmatized_words = [(wl.lemmatize(w),pos) for (w,pos) in words]
+        lemmatized_words = [wl.lemmatize(w) for w in words]
         #print("\n lemmatized words: ", lemmatized_words) # used for debugging purposes
 
         #Snippet to handle stopwords
         stopWords = stopwords.words('english') #store stopwords in stopWords
-        wl_noStopWords = [(w,pos) for (w,pos) in stemmed_words if w not in stopWords] #if w not in stopwords, append to wl_noStopWords
+        wl_noStopWords = [w for w in stemmed_words if w not in stopWords] #if w not in stopwords, append to wl_noStopWords
 
         # Snippet to sort the words and removes duplicates
         unique_words = sorted(set(wl_noStopWords))
 
-        freq_words = [(item[0],item[1],wl_noStopWords.count(item)) for item in unique_words]
-        print("freq_words: ", freq_words)#used for debugging purposes
-        return freq_words
+        freq_words = [(term,wl_noStopWords.count(term)) for term in unique_words]
+        #print("freq_words: ", freq_words)#used for debugging purposes
+        return freq_words #tuple consisting of (term,frequency)
 
 
     #function to calculate the term frequency
@@ -64,7 +64,7 @@ class DataProcessor:
 
         print('Query terms', term_freq_query)
         # find terms in both query and document
-        common_terms = [term for (term, pos, freq) in term_freq_query if term in doc_freq]
+        common_terms = [term for (term, freq) in term_freq_query if term in doc_freq]
         print("common terms: ", common_terms)
 
         # initialize similarity list to 0
@@ -81,7 +81,7 @@ class DataProcessor:
             # tf transformation = log2(1+tf(t)) in each document
 
             for doc in range(nr_docs):
-                tf_q = [f for (t,p,f) in term_freq_query if t in common_terms] #t = term, f = frequency p = part of speech
+                tf_q = [f for (t,f) in term_freq_query if t in common_terms] #t = term, f = frequency p = part of speech
                 tf_d = []  # for all common terms, extract the frequency of the term in doc, for the documents the term doesn't appear, the frequency is 0
                 for t in common_terms:
                     for (d, f) in term_freq_doc.get(t):
@@ -109,39 +109,57 @@ class DataProcessor:
         print("generating inverted index")
         doc_amt = len(doc_list) # holds the length of the doc_list
 
+        doc_list_str = str(doc_list)
+
         doc_term_frequency = []
+        #term_frequency = self.process(doc_list)
         for doc in range(doc_amt):
             term_frequency = self.process(doc_list[doc]) #tuple consists of (term, part of speech, frequency of the term)
 
             #output below used for debugging purposes
             #print("\n\nnext document to be processed", term_frequency)
-            doc_term_frequency += [(term,doc,frequency) for (term,pos,frequency) in term_frequency] #tuple consists of (term, document id, term frequency)
+            doc_term_frequency += [(term,doc,frequency) for (term,frequency) in term_frequency] #tuple consists of (term, document id, term frequency)
 
             #used for debugging purposes
             #print("\n All terms in document:" , doc_term_frequency)
 
             #list of terms and frequences
 
-            all_terms = [term for term,doc,frequency in doc_term_frequency]
-            unique_terms = sorted(set(all_terms))
+        all_terms = [term for term,doc,frequency in doc_term_frequency]
+        unique_terms = sorted(set(all_terms))
 
-            #doc frequency expressed as a dictionary
-            document_frequency = dict([(term,all_terms.count(term)) for term in unique_terms])
+        #doc frequency expressed as a dictionary
+        document_frequency = dict([(term,all_terms.count(term)) for term in unique_terms])
 
-            term_frequency_document = {} #initialized as an empty dictionary
+        term_frequency_document = {} #initialized as an empty dictionary
 
-            for (term,doc,frequency) in doc_term_frequency:
-                if term in term_frequency_document:
-                    term_frequency_document[term].append((doc,frequency))
-                else:
-                    term_frequency_document[term] = [(doc,frequency)]
+        for (term,doc,frequency) in doc_term_frequency:
+            if term in term_frequency_document:
+                term_frequency_document[term].append((doc,frequency))
+            else:
+                term_frequency_document[term] = [(doc,frequency)]
 
-            #return as a tuple
-            print("document_frequency: ", document_frequency)
-            print("term_frequency_document: ",term_frequency_document)
+        #return as a tuple
+        print("document_frequency: ", document_frequency)
+        print("term_frequency_document: ",term_frequency_document)
 
-            return document_frequency, term_frequency_document
+        return document_frequency, term_frequency_document
 
+    def get_doc_length(self,docs):#helper function to get length of documents
+        # function determining length of the document
+        # pass in a list of documents
+
+        lengths = []  # empty list
+        wordCount = 0  # word counter
+        docID = 0 #counter for document id
+        for document in docs:  # iterate through the list of documents
+            term_list = document.split(" ")
+            for term in term_list:
+                wordCount += 1
+            lengths.append(tuple((docID, wordCount)))
+            wordCount = 0
+            docID+=1
+        return lengths
 
     #function to return the min max average of the documents
     def minMaxAverage(docTuples):  # takes in a list of tuples
@@ -165,7 +183,7 @@ class DataProcessor:
     def process_texts(self,docs): #function that will read from the file or dataset
         print("Processing datasets")
         doc_list = []
-        for doc in doc_list:
+        for doc in docs:
             file = open(doc,"r",encoding='utf-8').read()
             doc_list.append(file)
 
@@ -181,6 +199,8 @@ class DataProcessor:
         print("Query terms ", term_freq_query)
 
         common_terms = [term for (term, freq) in term_freq_query if term in doc_freq]
+        print("common terms: ", common_terms)
+
 
         similarity = {i: 0 for i in range(nr_docs)}
 
@@ -191,7 +211,7 @@ class DataProcessor:
             print("IDF Query terms ", idf_query)
 
             for doc in range(nr_docs):
-                tf_q = [f for (t, f) in term_freq_query if t in common_terms]
+                tf_q = [f for (t,f) in term_freq_query if t in common_terms]
                 tf_d = []  # for all common terms, extract the frequency of the term in doc, for the documents the term doesn't appear, the frequency is 0
                 for t in common_terms:
                     for (d, f) in term_freq_doc.get(t):
@@ -205,13 +225,14 @@ class DataProcessor:
                     tf_idf_d = tf_q[c] * (((k + 1) * tf_d[c]) / (tf_d[c] + k)) * idf_query[c]  # k=10
                     similarity[doc] += tf_idf_d
         sorted_doc_list = sorted(similarity, key=similarity.get, reverse=True)
-        print("similarity: ", similarity)#used for debugging purposes
-        print("sorted_doc_list: ", sorted_doc_list)#used for debugging purposes
+        # print("similarity: ", similarity)#used for debugging purposes
+        # print("sorted_doc_list: ", sorted_doc_list)#used for debugging purposes
         return similarity, sorted_doc_list # returns a tuple
 
     #implement query likelyhood method with slide query likelyhood
-    def query_likelyhood_method(self,query,doc_list):
+    def query_likelyhood_method(self,query,doc_list,doc_lengths):
         num_docs = len(doc_list) # holds the number of documents passed in
+
 
 
     #function that implements rocchios algorithm
--- DataSet.py
+++ DataSet.py
@@ -9,28 +9,38 @@ def main():
     while not q:
         q = input("no empty queries please> ")
 
+    dp = DataProcessor()
+    # list_doc = dp.process_texts(sys.argv[1:])
+
+
     reuters_texts = []
     #Working with the first 50 files from the reuters library
-    dp = DataProcessor()
     reuters_data = reuters.fileids()[:50]
     for data in reuters_data:
         file_str = "" #concatinate file to string
         file = reuters.open(data)
         for line in file:
             file_str = file_str + line
+        file_str = file_str.replace('\n','')
+        file_str = file_str.replace("  "," ")
+        file_str = file_str.replace("   ", " ")
         reuters_texts.append(file_str)
 
-    for text in reuters_texts:
-        print(str(text)+"\n")
-    #print(reuters_texts) # used for debugging purposes
+    # for text in reuters_texts:
+    #     print(str(text)+"\n")
+    # #print(reuters_texts) # used for debugging purposes
 
     [document_frequency, term_frequency_document] = dp.inverted_index(reuters_texts)
     """returns the document frequency and term frequency document
     ITS A MUST WHEN CALCULATING THE TF-IDF
     """
-    [similarity,sorted_doc_list] = dp.tf_idf(reuters_texts,document_frequency,term_frequency_document,q)
+    # print("document_frequency: ", document_frequency)
+    # print("term_frequency_document: ",term_frequency_document)
+    [similarity,sorted_doc_list] = dp.bm25(reuters_texts,document_frequency,term_frequency_document,q)
+    document_lengths = dp.get_doc_length(reuters_texts)
 
-    print("similarity: ", similarity)
+    print("document lengths: " ,document_lengths)
+    print("using bm25 smoothing: ", similarity)
     print("sorted_doc_list: ",sorted_doc_list)
 
 
